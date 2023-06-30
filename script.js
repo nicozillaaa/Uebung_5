55eb994f875c99786d82d5a0497118714cc2ac03
nicozillaaa <137768406+nicozillaaa@users.noreply.github.com>
2023-06-30 11:39:18 +0200

    4 files modified

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+function handleSubmit() {
+  var name = document.getElementById('name').value;
+  var date = document.getElementById('date').value;
+  var address = document.getElementById('address').value;
+  var pet = document.getElementById('pet').value;
+  var check = document.getElementById('check').checked;
+
+  var message =
+    'Folgende Angaben wurden an uns übersandt:' +
+    '\nIhr Name:' +
+    name +
+    '\nFür den Zeitraum:' +
+    date +
+    '\nAdresse:' +
+    address +
+    '\nReist ihr Haustier mit?' +
+    pet +
+    '\nVielen Dank für Ihre Anfrage.';
+
+  alert(message);
+}
